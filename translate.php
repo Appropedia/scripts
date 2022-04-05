eb03ab2550b01c9b4fb98f5396e1a899ec9b6021
root <root@host.appropedia.org>
2022-04-05 09:54:31 -0500

    Major improvements and simplifications

--- translate.php
+++ translate.php
@@ -1,107 +1,118 @@
 <?php
 
-require_once __DIR__ . '/w/maintenance/Maintenance.php';
+// Set paths
+$EasyWiki = '/home/appropedia/EasyWiki/EasyWiki.php';
+$MediaWikiAPI = 'https://www.appropedia.org/w/api.php';
+$GoogleCloudSDK = '/home/appropedia/google-cloud-sdk/bin/gcloud';
 
-use MediaWiki\MediaWikiServices;
+// Extract the script options
+$options = getopt( 't:l:u:p:', [ 'title:', 'language:', 'user:', 'pass:' ] );
+list( $title, $language, $user, $pass ) = array_values( $options );
 
-class AppropediaTranslatorScript extends Maintenance {
+// Initialize EasyWiki
+require_once $EasyWiki;
+$wiki = new EasyWiki( $MediaWikiAPI, $user, $pass );
 
-	public function __construct() {
-		parent::__construct();
-		$this->addDescription( 'Translate a page using Google Translate' );
-		$this->addArg( 0, 'Title of the page to translate', false );
-	}
+// Get the wikitext of the page
+$wikitext = $wiki->getWikitext( $title );
+//var_dump( $wikitext ); exit; // Uncomment to debug
 
-	public function execute() {
+// Convert the wikitext to HTML
+$query = [
+	'action' => 'visualeditor',
+	'paction' => 'parsefragment',
+	'page' => $title,
+	'wikitext' => $wikitext,
+];
+$html = $wiki->get( $query, 'content' );
+//var_dump( $html ); exit; // Uncomment to debug
 
-		$page = $this->getArg();
+// Remove unwanted HTML to reduce characters sent to Google Translate
+$DOM = new DOMDocument;
+$DOM->loadHTML( $html );
+$xPath = new DomXPath( $DOM );
 
-		$language = 'es';
+// Remove unwanted nodes
+$unwantedNodes = '//style';
+foreach ( $xPath->query( $unwantedNodes ) as $unwantedNode ) {
+	$unwantedNode->parentNode->removeChild( $unwantedNode );
+}
 
-		//$wiki = new EasyWiki( 'https://mediawiki.solutions/w/api.php', 'Sophivorus@Bot', '0vf4jntsihi1sf3jocinu0r3is8e7usf' );
-		$wiki = new EasyWiki( 'https://www.appropedia.org/w/api.php', 'Sophivorus@Ant_bot', '7v9lkpmqpj8ieo8bed7co1u1h4r77522' );
-
-		// Get the wikitext of the page
-		$wikitext = $wiki->getWikitext( $page );
-		var_dump( $wikitext ); exit; // Uncomment to debug
-
-		// Convert the wikitext into HTML
-		$query = [
-			'action' => 'visualeditor',
-			'paction' => 'parsefragment',
-			'page' => $page,
-			'wikitext' => $wikitext,
-		];
-		$html = $wiki->get( $query, 'content' );
-		//var_dump( $html ); exit; // Uncomment to debug
-
-		// Remove unnecessary markup
-		// @todo Proper DOM parsing
-		$DOM = new DOMDocument;
-		$html = preg_replace( "/ data-parsoid='[^']+'/", '', $html );
-		$html = preg_replace( "/<style.*?>.*?<\/style>/s", '', $html ); 
-		$html = preg_replace( "/<div(.*?)data-mw(.*?)>.*<\/div>/s", '<div$1data-mw$2></div>', $html ); // @todo nested divs
-		//var_dump( $html ); exit; // Uncomment to debug
-
-		// Translate the HTML
-		$translation = $this->googleTranslate( $html, $language );
-		//var_dump( $translation ); exit; // Uncomment to debug
-
-		// Save the translated HTML
-		$query = [
-			'formatversion' => 2,
-			'action' => 'visualeditoredit',
-			'paction' => 'save',
-			'page' => "$page/$language",
-			'html' => $translation,
-		];
-		$data = $wiki->post( $query );
-		//var_dump( $data ); exit;
-	}
-
-	/**
-	 * Send strings to Google Translate
-	 */
-	public function googleTranslate( $contents, $targetLanguageCode ) {
-		global $wgGoogleCloudSDK;
-
-		// Build the request
-		$payload = json_encode( [
-			'target_language_code' => $targetLanguageCode,
-			'contents' => $contents
-		] );
-
-		$token = exec( "$wgGoogleCloudSDK auth print-access-token" );
-
-		$headers = [
-			"Authorization: Bearer $token",
-			'Content-Type: application/json'
-		];
-
-		// Do the request
-		$curl = curl_init( 'https://translation.googleapis.com/v3beta1/projects/wikimix/locations/global:translateText' );
-		curl_setopt( $curl, CURLOPT_POST, true );
-		curl_setopt( $curl, CURLOPT_POSTFIELDS, $payload );
-		curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers );
-		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-		$json = curl_exec( $curl );
-		curl_close( $curl );
-
-		// Process the output and return it
-		$data = json_decode( $json );
-		if ( ! property_exists( $data, 'translations' ) ) {
-			var_dump( $data );
-			exit;
-		}
-
-		// Unwrap and return the translation
-		$translation = $data->translations;
-		$translation = array_shift( $translation );
-		$translation = $translation->translatedText;
-		$translation = html_entity_decode( $translation, ENT_QUOTES, 'UTF-8' );
-		return $translation;
+// Remove unwanted attributes
+$unwantedAttributes = [ 'data-parsoid' ];
+foreach ( $xPath->query( '//*' ) as $node ) {
+	foreach ( $unwantedAttributes as $unwantedAttribute ) {
+		$node->removeAttribute( $unwantedAttribute );
 	}
 }
 
-$maintClass = AppropediaTranslatorScript::class;
-require_once RUN_MAINTENANCE_IF_MAIN;
+// Remove unwanted children
+$parentNodes = '//*[@data-mw]';
+foreach ( $xPath->query( $parentNodes ) as $parentNode ) {
+	while ( $parentNode->hasChildNodes() ) {
+		$parentNode->removeChild( $parentNode->firstChild );
+	}
+}
+
+// Get the reduced HTML
+$html = $DOM->saveHTML();
+//var_dump( $html ); exit; // Uncomment to debug
+
+// Translate the HTML
+$translation = googleTranslate( $html, $language );
+//var_dump( $translation ); exit; // Uncomment to debug
+
+// Save the translated HTML
+$query = [
+	'formatversion' => 2,
+	'action' => 'visualeditoredit',
+	'paction' => 'save',
+	'page' => "$title/$language",
+	'html' => $translation,
+];
+$data = $wiki->post( $query );
+//var_dump( $data ); exit;
+
+/**
+ * Translate HTML using Google Translate
+ */
+function googleTranslate( $html, $targetLanguageCode ) {
+	global $GoogleCloudSDK;
+
+	// Get the authorization token
+	$token = exec( "$GoogleCloudSDK auth print-access-token" );
+
+	// Build the request
+	$payload = json_encode( [
+		'target_language_code' => $targetLanguageCode,
+		'contents' => $html
+	] );
+
+	$headers = [
+		"Authorization: Bearer $token",
+		'Content-Type: application/json'
+	];
+
+	// Do the request
+	$curl = curl_init( 'https://translation.googleapis.com/v3beta1/projects/wikimix/locations/global:translateText' );
+	curl_setopt( $curl, CURLOPT_POST, true );
+	curl_setopt( $curl, CURLOPT_POSTFIELDS, $payload );
+	curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers );
+	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+	$json = curl_exec( $curl );
+	curl_close( $curl );
+
+	// Process the output and return it
+	$data = json_decode( $json );
+	if ( ! property_exists( $data, 'translations' ) ) {
+		var_dump( $data );
+		exit;
+	}
+
+	// Unwrap and return the translation
+	$translation = $data->translations;
+	$translation = array_shift( $translation );
+	$translation = $translation->translatedText;
+	$translation = html_entity_decode( $translation, ENT_QUOTES, 'UTF-8' );
+	return $translation;
+}
