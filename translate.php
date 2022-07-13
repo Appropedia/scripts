e335f8eaa0f38e16c1589c9da3332a7e72c44c2f
root <root@host.appropedia.org>
2022-07-13 18:42:12 -0500

    Translate template parameters too

--- translate.php
+++ translate.php
@@ -54,25 +54,40 @@ $params = [
 $html = $wiki->post( $params, 'content' );
 //var_dump( $html ); exit; // Uncomment to debug
 
+// Ugly hack to fix encoding issue when template parameters have single quotes
+// @todo Also fix encoding issue when template parameters have double quotes
+// @note Maybe fix at the wikitext stage?
+$html = preg_replace_callback( '/data-mw="(.*?)">/s', function ( $matches ) {
+	$content = $matches[1];
+	$content = str_replace( "'", '&apos;', $content );
+	$content = str_replace( '&quot;', '"', $content );
+	return "data-mw='$content'>";
+}, $html );
+//var_dump( $html ); exit; // Uncomment to debug
+
 // Remove unwanted HTML to reduce characters sent to Google Translate
 $DOM = new DOMDocument;
+$html = mb_convert_encoding( $html, 'HTML-ENTITIES', 'UTF-8' );
 libxml_use_internal_errors( true ); // Temporarily disable error reporting because DOMDocument complains about HTML5 tags
 $DOM->loadHTML( $html );
 libxml_clear_errors();
 $xPath = new DomXPath( $DOM );
 
 // Remove unwanted nodes
-$unwantedNodes = '//style | //link';
+$unwantedNodes = '//style | //link | //meta';
 foreach ( $xPath->query( $unwantedNodes ) as $unwantedNode ) {
 	$unwantedNode->parentNode->removeChild( $unwantedNode );
 }
 
 // Remove unwanted attributes
-$unwantedAttributes = [ 'data-parsoid' ];
+$unwantedAttributes = [ 'data-parsoid', 'style', 'about', 'src', 'srcset' ];
 foreach ( $xPath->query( '//*' ) as $node ) {
 	foreach ( $unwantedAttributes as $unwantedAttribute ) {
 		$node->removeAttribute( $unwantedAttribute );
 	}
+}
+foreach ( $xPath->query( '//h1 | //h2 | //h3 | //h4 | //h5 | //h6' ) as $node ) {
+	$node->removeAttribute( 'id' );
 }
 
 // Remove unwanted children
@@ -90,19 +105,32 @@ foreach ( $xPath->query( $parentNodes ) as $parentNode ) {
 	}
 }
 
+// Translate some template parameters
+$translatableParams = [ 'title', 'text', 'content' ];
+$nodes = '//*[ @data-mw ]';
+foreach ( $xPath->query( $nodes ) as $node ) {
+	$data = $node->getAttribute( 'data-mw' );
+	$data = json_decode( $data, true );
+	$params = $data['parts'][0]['template']['params'];
+	if ( !$params ) {
+		continue;
+	}
+	foreach ( $params as $param => $value ) {
+		if ( in_array( $param, $translatableParams ) ) {
+			$params[ $param ]['wt'] = googleTranslate( $value['wt'], $language );
+		}
+	}
+	$data['parts'][0]['template']['params'] = $params;
+	$data = json_encode( $data );
+	$node->setAttribute( 'data-mw', $data );
+}
+
 // Get the reduced HTML
 $html = $DOM->saveHTML();
-//var_dump( $html ); exit; // Uncomment to debug
-
-// Ugly hack to fix encoding issue when template parameters have single quotes
-// @todo Also fix encoding issue when template parameters have double quotes
-// @note Maybe fix at the wikitext stage?
-$html = preg_replace_callback( '/data-mw="(.*?)"/s', function ( $matches ) {
-	$content = $matches[1];
-	$content = str_replace( "'", '&apos;', $content );
-	$content = str_replace( '&quot;', '"', $content );
-	return "data-mw='$content'";
-}, $html );
+$html = html_entity_decode( $html );
+if ( strlen( $html ) > 30000 ) {
+	echo 'Page too long' . PHP_EOL; exit;
+}
 //var_dump( $html ); exit; // Uncomment to debug
 
 // Translate the HTML
@@ -130,6 +158,14 @@ function googleTranslate( $html, $targetLanguageCode ) {
 	$GoogleTranslateClient = new TranslationServiceClient( [
 		'credentials' => $wgGoogleCloudKey
 	] );
+
+	// Keep track of the translated characters
+	$chars = file_get_contents( 'translate' );
+	$chars += strlen( $html );
+	if ( $chars > 5000000 ) {
+		return;
+	}
+	file_put_contents( 'translate', $chars );
 
 	try {
 		$contents = [ $html ];
