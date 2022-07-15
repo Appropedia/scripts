d9f95824b1c39446ed25809288b8be06dff5323b
root <root@host.appropedia.org>
2022-07-15 11:49:40 -0500

    Refinements

--- listTemplatesByUse.php
+++ listTemplatesByUse.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This script lists templates by use count
+ * This script lists the top 100 templates by use count
  */
 
 require_once __DIR__ . '/../w/maintenance/Maintenance.php';
@@ -13,9 +13,8 @@ class ListTemplatesByUse extends Maintenance {
 	public static $templates = [];
 
 	public function execute() {
-		$templates = [];
 		$dbr = wfGetDB( DB_REPLICA );
-		$result = $dbr->select( 'page', 'page_id', [ 'page_namespace' => 0, 'page_is_redirect' => 0 ] );
+		$result = $dbr->select( 'page', 'page_id', [ 'page_is_redirect' => 0 ] );
 		foreach ( $result as $row ) {
 			$id = $row->page_id;
 			$Title = Title::newFromID( $id );
@@ -23,24 +22,38 @@ class ListTemplatesByUse extends Maintenance {
 				continue;
 			}
 			$Page = WikiPage::factory( $Title );
+			if ( ! $Page ) {
+				continue;
+			}
 			$Revision = $Page->getRevision();
+			if ( ! $Revision ) {
+				continue;
+			}
 			$Content = $Revision->getContent( Revision::RAW );
+			if ( ! $Content ) {
+				continue;
+			}
 			$text = ContentHandler::getContentText( $Content );
+			if ( ! $text ) {
+				continue;
+			}
 
-			preg_replace_callback( '/{{([^#}|]+)/', function ( $matches ) {
+			preg_replace_callback( '/[^{]{{([^#}{|]+)/', function ( $matches ) {
 				$template = $matches[1];
-				$template = ucfirst( trim( $template ) );
+				$template = trim( $template );
+				$template = ucfirst( $template );
+				$template = str_replace( '_', ' ', $template );
 				$count = self::$templates[ $template ] ?? 0;
 				$count++;
 				self::$templates[ $template ] = $count;
-			}, $text );
-
-			//break; // Uncomment to debug
+			}, '@' . $text );
 		}
 		$templates = self::$templates;
-		arsort( $templates );
-		$templates = array_splice( $templates, 0, 100 );
-		var_dump( $templates );
+		asort( $templates );
+		//$templates = array_splice( $templates, 0, 100 );
+		foreach ( $templates as $template => $count ) {
+			echo $count . ' ' . $template . PHP_EOL;
+		}
 	}
 }
 
