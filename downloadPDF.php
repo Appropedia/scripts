e936d88cdb3cbce8c619d978371380b9e3657380
Sophivorus <schenonef@gmail.com>
2025-03-12 11:16:08 -0500

    Add PDF generation

---
+++ downloadPDF.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * This script generates and downloads a PDF containing a specified set of pages
+ * It uses the wkhtmltopdf library, see https://wkhtmltopdf.org/usage/wkhtmltopdf.txt
+ */
+
+// Debug mode
+$debug = $_GET['debug'] ?? false;
+if ( $debug ) {
+	error_reporting( E_ALL );
+	ini_set( 'display_errors', 1 );
+}
+
+// Load the library for generating QR codes
+require 'vendor/autoload.php';
+use chillerlan\QRCode\QRCode;
+
+// Get the params
+$title = $_GET['title'] ?? null;
+$subtitle = $_GET['subtitle'] ?? null;
+$text = $_GET['text'] ?? null;
+$logo = $_GET['logo'] ?? null;
+$logowidth = $_GET['logowidth'] ?? null;
+$qrpage = $_GET['qrpage'] ?? null;
+$pages = $_GET['pages'] ?? null;
+
+// If no pages are given, use Appropedia's main page to avoid throwing an error
+if ( !$pages ) {
+	$pages = 'Welcome to Appropedia';
+}
+
+// Start building the command
+$command = 'wkhtmltopdf';
+$command .= ' --user-style-sheet downloadPDF.css';
+
+// Set the cover
+if ( $title ) {
+	$html = '<!DOCTYPE HTML>';
+	$html .= '<html>';
+	$html .= '<head>';
+	$html .= '<meta charset="utf-8">';
+	$html .= '<title>' . $title . '</title>';
+	$html .= '</head>';
+	$html .= '<body id="cover">';
+	$html .= '<header>';
+	if ( $logo ) {
+		$html .= '<img id="cover-logo" src="' . $logo . '" width="' . $logowidth . '" />';
+	}
+	$html .= '<h1 id="cover-title">' . $title . '</h1>';
+	if ( $subtitle ) {
+		$html .= '<p id="cover-subtitle">' . $subtitle . '</p>';
+	}
+	$html .= '</header>';
+	if ( $text ) {
+		$html .= '<p id="cover-text">' . $text . '</p>';
+	}
+	if ( $qrpage ) {
+		$qrpage = str_replace( ' ', '_', $qrpage );
+		$qrcode = new QRCode;
+		$src = $qrcode->render( 'https://www.appropedia.org/' . $qrpage );
+		$html .= '<img id="cover-qrcode" src="' . $src . '" />';
+	}
+	$html .= '<footer>';
+	$html .= '<img id="cover-appropedia" src="https://www.appropedia.org/logos/Appropedia-logo.png" />';
+	$html .= '</footer>';
+	$html .= '</body>';
+	$html .= '</html>';
+	file_put_contents( 'cover.html', $html );
+	$command .= ' cover cover.html';
+}
+
+// Set the pages
+$pages = explode( ',', $pages );
+foreach ( $pages as $page ) {
+	$page = trim( $page );
+	$page = str_replace( ' ', '_', $page );
+	$page = urlencode( $page );
+	$url = "https://www.appropedia.org/$page";
+    $command .= " $url";
+}
+
+// Set the output
+$command .= ' temp.pdf';
+exec( $command );
+
+// Download the PDF
+$filename = $title ? $title : 'appropedia';
+header( 'Content-Type: application/pdf' );
+header( 'Content-Disposition: attachment; filename=' . $filename . '.pdf' );
+readfile( 'temp.pdf' );
+
+// Clean up
+unlink( 'temp.pdf' );
+unlink( 'cover.html' );
--- downloadZIM.php
+++ downloadZIM.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * This script generates and downloads a ZIM file containing a specified set of pages
+ * It uses the mwoffliner library to generate the ZIM file, see https://github.com/openzim/mwoffliner
+ * and EasyWiki to interact with the Appropedia API, see https://github.com/Sophivorus/EasyWiki
+ */
+
 // Debug mode
 $debug = $_GET['debug'] ?? false;
 if ( $debug ) {
@@ -7,54 +13,41 @@ if ( $debug ) {
 	ini_set( 'display_errors', 1 );
 }
 
+// Load EasyWiki
 require 'vendor/autoload.php';
 
-// Connect to the Appropedia API
-$api = new EasyWiki( 'https://www.appropedia.org/w/api.php' );
-
-$category = $_GET['category'] ?? null;
-
-$pages = $_GET['pages'] ?? null;
-
-$title = $_GET['title'] ?? $category ?? null;
-$title = str_replace( '_', ' ', $title );
-$titlee = str_replace( ' ', '_', $title );
-
-$filenamePrefix = strtolower( $titlee );
-
-// Build the query
-if ( $category ) {
-	$params = [
-		'generator' => 'categorymembers',
-		'gcmtitle' => 'Category:' . $category,
-		'gcmtype' => 'page',
-		'gcmlimit' => 'max',
-	];
+// Get the pages
+if ( !array_key_exists( 'pages', $_GET ) ) {
+	exit;
 }
+$pages = $_GET['pages'];
 
-// Get all the titles
+// Query the API
+$pages = str_replace( ',', '|', $pages );
+$params = [ 'titles' => $pages ];
+$api = new EasyWiki( 'https://www.appropedia.org/w/api.php' );
 $result = $api->query( $params );
 //echo '<pre>'; var_dump( $result ); exit; // Uncomment to debug
-$titles = $api->find( 'title', $result );
-while ( $continue = $api->find( 'gcmcontinue', $result ) ) {
-	$params['gcmcontinue'] = $continue;
-	$result = $api->query( $params );
-	$moreTitles = $api->find( 'title', $result );
-	$titles = array_merge( $titles, $moreTitles );
-}
 
 // Replace spaces for understcores or mwoffliner won't work
+$titles = $api->find( 'title', $result );
+if ( is_string( $titles ) ) {
+	$titles = [ $titles ];
+}
 $titles = array_map( function ( $title ) {
 	return str_replace( ' ', '_', $title );
 }, $titles );
 //echo '<pre>'; var_dump( $titles ); exit; // Uncomment to debug
 
+// Build the mwoffliner command
+$title = $_GET['title'] ?? 'appropedia';
+$titlee = str_replace( ' ', '_', $title );
 $command = 'mwoffliner';
 $command .= ' --adminEmail=admin@appropedia.org';
 $command .= ' --customZimDescription="Shared knowledge to build rich, sustainable lives"';
-$command .= ' --customZimFavicon=https://www.appropedia.org/logos/Appropedia-zim.png';
+$command .= ' --customZimFavicon=https://www.appropedia.org/logos/Appropedia-kiwix.png';
 $command .= ' --customZimTitle="' . $title . '"';
-$command .= ' --filenamePrefix=' . $filenamePrefix;
+$command .= ' --filenamePrefix=' . $titlee;
 $command .= ' --mwUrl=https://www.appropedia.org/';
 $command .= ' --outputDirectory=/home/appropedia/public_html/scripts';
 $command .= ' --osTmpDir=/home/appropedia/public_html/scripts';
@@ -63,13 +56,13 @@ $command .= ' --requestTimeout=120';
 $command .= ' --webp';
 $command .= ' --articleList=' . implode( ',', $titles );
 $command .= ' --forceRender=RestApi';
-$command .= ' --verbose';
-
+//$command .= ' --verbose';
 exec( $command, $output );
-//echo '<pre>' . $command . '<br><br>'; var_dump( $output ); exit;
+//echo '<pre>' . $command . '<br><br>'; var_dump( $output ); exit; // Uncomment to debug
 
+// Download the ZIM file
 $date = date( 'Y-m' );
-$filename = $filenamePrefix . '_' . $date . '.zim';
+$filename = $titlee . '_' . $date . '.zim';
 header( 'Content-Type: application/octet-stream' );
 header( 'Content-Disposition: attachment; filename=' . $filename );
 readfile( $filename );
